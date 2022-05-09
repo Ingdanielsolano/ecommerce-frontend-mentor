3c1cd7435b78e29e7b182d8680cdf7fa766bc71d
IngDanielSolano <ds.development.app@gmail.com>
2022-05-08 22:09:35 -0500

    feat(public): sidebar

--- layout/public.js
+++ layout/public.js
@@ -1,29 +1,66 @@
 import Image from "next/image";
+import Link from "next/link";
 import propTypes from "prop-types";
 import MenuIconSvg from "@public/icons/icon-menu.svg";
 import LogoSvg from "@public/images/logo.svg";
 import CartIconSvg from "@public/icons/icon-cart.svg";
+import CloseIconSvg from "@public/icons/icon-close.svg";
 import AvatarPng from "@public/images/image-avatar.png";
+import { useState } from "react";
 
-const PublicLayout = ({ children }) => (
-  <div className="public-layout">
-    <nav className="public-layout__navbar">
-      <div className="public-layout__wrapper public-layout__wrapper--left">
-        <div>
-          <Image src={MenuIconSvg} width="20" height="16" alt="Menu" />
+const PublicLayout = ({ children }) => {
+  const [openSidebar, setOpenSidebar] = useState();
+  return (
+    <div className="public-layout">
+      <nav className="public-layout__navbar">
+        <div className="public-layout__wrapper public-layout__wrapper--left">
+          <div>
+            <Image
+              onClick={() => setOpenSidebar(!openSidebar)}
+              src={MenuIconSvg}
+              width="20"
+              height="16"
+              alt="Menu"
+            />
+          </div>
+          <div className="logo">
+            <Image src={LogoSvg} width="146" height="24" alt="Logo" />
+          </div>
         </div>
-        <div className="logo">
-          <Image src={LogoSvg} width="146" height="24" alt="Logo" />
+        <div className="public-layout__wrapper public-layout__wrapper--rigth">
+          <Image src={CartIconSvg} width="24" height="24" alt="Logo" />
+          <Image src={AvatarPng} width="27" height="27" alt="Logo" />
+        </div>
+      </nav>
+      <div
+        className={`public-layout__sidenav-wrapper public-layout__sidenav-wrapper${
+          openSidebar ? "--open" : "--closed"
+        }`}
+      >
+        <div
+          className={`public-layout__sidenav public-layout__sidenav${
+            openSidebar ? "--open" : "--closed"
+          }`}
+        >
+          <button onClick={() => setOpenSidebar(!openSidebar)}>
+            <Image
+              src={CloseIconSvg}
+              width="14"
+              height="14"
+              alt="Close sidebar"
+            />
+          </button>
+          <Link href="/product">Collections</Link>
+          <Link href="/product">Men</Link>
+          <Link href="/product">Women</Link>
+          <Link href="/product">About</Link>
+          <Link href="/product">Contact</Link>
         </div>
       </div>
-      <div className="public-layout__wrapper public-layout__wrapper--rigth">
-        <Image src={CartIconSvg} width="24" height="24" alt="Logo" />
-        <Image src={AvatarPng} width="27" height="27" alt="Logo" />
-      </div>
-    </nav>
-    <main>{children}</main>
-  </div>
-);
+      <main>{children}</main>
+    </div>
+  );
+};
 
 PublicLayout.propTypes = {
   children: propTypes.node,
